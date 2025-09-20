35bb2e0ad2e688ea4907d31d655158a3366fea08
NarendraSaktiWw <yurayurawa@gmail.com>
2025-09-20 11:58:15 +0700

    laporanhariini

--- pages/LOGIN.jsx
+++ pages/LOGIN.jsx
@@ -1,65 +1,119 @@
-import React, { useState } from "react";
-import App from "../src/App";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 function LoginPage() {
-    const [formData, setFormData] = useState ({
-        email: '',
-        password: '', 
-    });
+  const [formData, setFormData] = useState({ email: "", password: ""});
+  const navigate = useNavigate();
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value});
-    };
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('Login data:', formData);
-        alert('Simulasi Login Berhasil')
-    };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-    return (
-        <div className="flex item-center justify-center min-h-screen -bg-gray-100">
-        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
-            <h1 className="text-2x1 font-bold text-center mb-6">Login</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="email"
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onchange={handleChange}
-                    placeholder="Masukan email anda"
-                    required
-                    />
-                </div>
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2"htmlFor="Password">Password</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus"
-                    id="password"
-                    type="password"
-                    name="password"
-                    value={formData.password}
-                    onchange={handleChange}
-                    placeholder="Masukan Password Anda"
-                    required
-                    />
-                </div>
-                <div className="flex items-center justify-between">
-                    <button className="bg-blue-500 hover:bg-blue-70 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    type="submit"
-                    >
-                    Masuk
-                    </button>
-                    <Link to="/register" className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
-                    Belum punya akun? Daftar
-                    </Link>
-                </div>
-            </form>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.get("http://localhost:5000/users", {
+        params: { email: formData.email, password: formData.password },
+      })
+
+      if (res.data.length > 0) {
+        alert("login sukses!")
+        navigate("/tabeldata")
+      } else {
+        alert/"Email atau password salah!"
+      }
+      ('http://localhost:5000/users', formData);
+      console.log('Register success:', Response.data);
+
+      alert('Pendaftaran Berhasil!');
+      navigate('/login')
+    } catch (error) {
+        console.error('Error register:', error)
+        alert('Terjadi kesalahan saat mendaftar')
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex item-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md w-100 h-100 max-w-sn mt-30">
+        <h1 className="text-2x1 font-bold text-center mb-6">Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">                                                                                                                                                                        
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="Name"
+            >
+              nama
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="name"
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              placeholder="Masukan nama Anda"
+              required
+            />
           </div>
-        </div>
-    );
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Masukan email Anda"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className=" shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus"
+              id="password"
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Buat password Anda"
+              required
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-70 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Masuk
+            </button>
+            <Link
+              to="/registerPage"
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            >
+              Belum punya akun? Daftar
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { Routes, Route }from "react-router-dom";
 import Tabeltailwind from '../pages/tabeltailwind';
 import LOGIN from '../pages/LOGIN';
 import Landingpage from "../pages/Landingpage";
+import Register from "../pages/Register";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Landingpage/>}/>
+      <Route path="/" element={<LOGIN/>}/>
     </Routes>
   )
 }
